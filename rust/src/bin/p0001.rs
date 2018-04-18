485dc8a58d9fee607b8bae530b08292345242c01
Thomas Roulin <thomasroulinch@gmail.com>
2018-04-18 14:50:35 +0200

    Refoldering to add other languages

---
+++ rust/src/bin/p0001.rs
@@ -0,0 +1,7 @@
+fn compute(bound: u32) -> u32 {
+    (1..bound).filter(|&n| n % 3 == 0 || n % 5 == 0).sum()
+}
+
+fn main() {
+    println!("{:?}", compute(1000));
+}
---
+++ rust/target/debug/p0001.d
@@ -0,0 +1,1 @@
+/home/megaturbo/stuff/perso/project-euler/rust/target/debug/p0001: /home/megaturbo/stuff/perso/project-euler/rust/src/bin/p0001.rs
