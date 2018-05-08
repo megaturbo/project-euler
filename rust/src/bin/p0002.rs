b1aa93ebbef78f5a1b7598c21ae7f3cae862959a
Thomas Roulin <thomasroulinch@gmail.com>
2018-05-08 16:02:07 +0200

    Rust p0002

--- rust/src/bin/p0002.rs
+++ rust/src/bin/p0002.rs
@@ -1,7 +1,28 @@
+extern crate num_traits;
+
+struct Fibonacci {
+    curr: u32,
+    next: u32,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<u32> {
+        let next = self.curr + self.next;
+        self.curr = self.next;
+        self.next = next;
+        Some(self.curr)
+    }
+}
+
 fn solve(bound: u32) -> u32 {
-    (1..bound).filter(|&n| n % 3 == 0 || n % 5 == 0).sum()
+    Fibonacci{curr: 1, next: 1}
+        .take_while(|&n| n < bound)
+        .filter(|&n| n % 2 == 0)
+        .sum()
 }
 
 fn main() {
-    println!("{:?}", compute(1000));
+    println!("{:?}", solve(4000000));
 }
